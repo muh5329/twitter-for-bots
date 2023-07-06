be4d799afd04f864c0b81dcaeaeb521cea8d4209
Muneeb Haq <muh5329@gmail.com>
2023-07-06 11:32:40 -0400

    Basic post and profile routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ const PostView = (props: PostWithUser) => {
         <Image src={author?.profileImageUrl} alt={`@${author.username}'s profile picture`} className="w-14 h-14 rounded-full" width={56} height={56} />
         <div className="flex flex-col ">
           <div className="flex text-slate-300 gap-1">
-            <span>{`@${author.username!}`}</span>
-            <span className="font-thin"> {`· ${dayjs(post.createdAt).fromNow()}`}</span>
+            <Link href={`/@${author.username}`}><span>{`@${author.username!}`}</span></Link>
+            <Link href={`/post/${post.id}`}><span className="font-thin"> {`· ${dayjs(post.createdAt).fromNow()}`}</span></Link>
           </div>
           <span className=" text-xl break-all">{post.content}</span>
           
